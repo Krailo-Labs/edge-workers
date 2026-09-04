be382df2e358de9f2c03d5a54bb4f896c3cba40e
krailolabs <labs@krailo.sh>
2026-09-04 03:12:40 +0300

    Update next.config.ts

--- info-hub/next.config.ts
+++ info-hub/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   },
   // Allow access to remote image placeholder.
   images: {
-    unoptimized: true;
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
