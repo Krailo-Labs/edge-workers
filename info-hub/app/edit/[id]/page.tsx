3cd7f846769320cfdb0cdcf9b5d98e12d7f77e8a
krailolabs <labs@krailo.sh>
2026-09-05 03:31:42 +0300

    fx

--- info-hub/app/edit/[id]/page.tsx
+++ info-hub/app/edit/[id]/page.tsx
@@ -3,6 +3,8 @@
 import { useParams } from 'next/navigation';
 import { NotionEditor } from '@/features/editor/NotionEditor';
 
+export const runtime = 'edge';
+
 export default function EditPage() {
   const params = useParams();
   const id = params?.id as string;
